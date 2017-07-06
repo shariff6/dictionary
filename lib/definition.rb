79f44aad2c5a638afe464ca66fcfd5465ddc1e3a
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-06 23:52:02 +0300

    this commit will test the find level

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,13 +1,21 @@
 class Definition
-  attr_reader = (:definition)
-  @@definition = []
+  attr_reader = (:definition, :id)
+  @@definitions = []
   define_method(:initialize) do |definition|
     @definition = definition
+    @id = @@identification.lenght.+(1)
   end
   define_method(:save) do
-    @@definition.push(self)
+    @@definitions.push(self)
   end
-  define_method(:clear) do
+  define_singleton_method(:clear) do
     @@definition = []
+  end
+  define_singleton_method(:find) do |identity|
+    found_definition = nil
+    @@definitions.each do |definitions|
+      found_definition = identification if identificationto_i
+    end
+    found_definition
   end
 end
