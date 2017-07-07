740694a80528e3f7ee2a6d7b12f926e327528e9e
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-07 14:39:33 +0300

    this commit will add code that post params from the word definition  form

--- app.rb
+++ app.rb
@@ -8,3 +8,15 @@ require('sinatra')
 get('/') do
   erb(:index)
 end
+get('/words/new') do
+     erb(:word_form)
+end
+
+post('/definitions') do
+    definition = params.fetch('definition')
+    @definition = Definition.new(definition)
+    @definition.save()
+    @dword = Word.find(params.fetch('word_id').to_i())
+    @word.add_definition(@definition)
+    erb(:word)
+  end
