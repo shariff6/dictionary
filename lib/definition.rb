863b4d39b4ee3d7ff82ff30e1f8aa490eea51943
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-07 11:44:45 +0300

    this commit will correct more errors in the code

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,20 +1,23 @@
 class Definition
   @@definitions = []
-  attr_reader  :definition, :id
+  attr_reader(:definition, :id)
   define_method(:initialize) do |definition|
     @definition = definition
     @id = @@definitions.length.+(1)
+  end
+  define_singleton_method(:all) do
+    @@definitions
   end
   define_method(:save) do
     @@definitions.push(self)
   end
   define_singleton_method(:clear) do
-    @@definition = []
+    @@definitions = []
   end
   define_singleton_method(:find) do |identity|
     found_definition = nil
     @@definitions.each do |definitions|
-      found_definition = identification if identification.to_i
+      found_definition = definitions if definitions.id.eql?(identity.to_i)
     end
     found_definition
   end
