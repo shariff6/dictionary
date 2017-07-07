dd39374d1b25ff89607779f4d26f5b2b71655898
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-07 12:14:23 +0300

    this commit will add a test for the save method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -12,4 +12,11 @@ describe(Word) do
       expect(test_word.word).to(eq("Happy"))
     end
   end
+  describe("#save") do
+    it "saves a word to an array" do
+      test_word = Word.new("happy")
+      test_word.save
+      expect(Word.all).to(eq([test_word]))
+    end
+  end
 end
